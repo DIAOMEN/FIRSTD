8c0fc5e1589a89aac22b1ba80c7454b7a4bc16d8
xiehuihui <somegrey@qq.com>
2016-06-23 23:34:38 +0800

    Signed-off-by: xiehuihui <somegrey@qq.com>

--- src/main/java/org/diaomen/App.java
+++ src/main/java/org/diaomen/App.java
@@ -1,5 +1,7 @@
 package org.diaomen;
 
+import java.io.File;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-		System.out.println("Hello World!");
+		File file = new File("");
     }
 }
