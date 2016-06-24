ed5dbef45f01a25aae8b93183dbe6ac3cd7378f6
xiehuihui <somegrey@qq.com>
2016-06-24 21:38:51 +0800

    Signed-off-by: xiehuihui <somegrey@qq.com>

--- src/main/java/org/diaomen/App.java
+++ src/main/java/org/diaomen/App.java
@@ -1,15 +1,21 @@
 package org.diaomen;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.Reader;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-		File file = new File("");
-    }
+public class App {
+	public static void main(String[] args) throws Exception {
+		String project_path = System.getProperty("user.dir");
+		File in = new File(project_path + "/file/in");
+		File out = new File(project_path + "/file/out");
+		Reader reader = new FileReader(in);
+		if (reader.ready()) {
+			char[] cbuf = new char[100];
+			int i = -1;
+			while ((i = reader.read(cbuf)) != -1) {
+				System.out.print(new String(cbuf).substring(0, i));
+			}
+		}
+	}
 }
