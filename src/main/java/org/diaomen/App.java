da8a705b0abb1642fe151abe5fa809fddb015a80
xiehuihui <somegrey@qq.com>
2016-06-25 11:18:08 +0800

    Signed-off-by: xiehuihui <somegrey@qq.com>

--- src/main/java/org/diaomen/App.java
+++ src/main/java/org/diaomen/App.java
@@ -1,9 +1,44 @@
 package org.diaomen;
 
-import org.diaomen.util.CharacterProcess;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class App {
-	public static void main(String[] args) throws Exception {
-		CharacterProcess.setContent(null, CharacterProcess.getContent(null));
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class App extends HttpServlet{
+
+	/**
+	 * 
+	 * @create 2016年6月25日 上午10:38:43
+	 * @by xiehuihui
+	 * @param req
+	 * @param resp
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doPost(req,resp);
 	}
+
+	/**
+	 * 
+	 * @create 2016年6月25日 上午10:38:43
+	 * @by xiehuihui
+	 * @param req
+	 * @param resp
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter pw = resp.getWriter();
+		pw.println("hello servlet!");
+		pw.flush();
+		pw.close();
+	}
+	
 }
